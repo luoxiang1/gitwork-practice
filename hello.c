eaa001691e626ae0b0530dc36b3598841e5f932c
luoxiang <2272735437@qq.com>
2022-12-30 23:21:21 +0800

    生成可执行程序

--- hello.c
+++ hello.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 int main(){
-    printf("hellow world");
-    printf("we are the children");
+    printf("hellow world \n");
+    printf("we are the children\n");
 
     int a=100,b=200;
-    printf("%d",a+b);
-    printf("马上放元旦假期了");
+    printf("%d\n",a+b);
+    printf("it's time to 2023");
 
     return 0;
 }
